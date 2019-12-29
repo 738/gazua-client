24d22f30ad453ac611fde08a09ed618e39043e47
Jon Jee <jydrogen@gmail.com>
2019-12-29 18:02:02 +0700

    feat: add url input, button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [url, setUrl] = useState('');
+  const onChangeUrl = e => {
+    setUrl(e.target.value);
+  }
   return (
     <div className="App">
-      gazua-client
+      <input type="text" value={url} onChange={onChangeUrl} />
+      <button>generate short url</button>
     </div>
   );
 }
